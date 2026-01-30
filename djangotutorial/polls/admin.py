1f82e2d0cbe3b46c54afb5083d8c9c0959c77fc8
Your Name <you@example.com>
2026-01-30 16:55:21 +0530

    [IMP] database setup

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
-from .models import Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3   # shows 3 empty choice boxes
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
